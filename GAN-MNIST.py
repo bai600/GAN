12f6f4d9f3b5714fd791b98ecb16d257fb88e307
bai <443384632@qq.com>
2022-02-16 21:50:22 +0800

    path

--- GAN-MNIST.py
+++ GAN-MNIST.py
@@ -9,7 +9,7 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 #读取数据
 def load_mnist():
-    path = r'.\mnist.npz' #放置mnist.py的目录
+    path =  r'.\GAN-images\mnist.npz' #放置mnist.py的目录
     f = np.load(path)
     x_train, y_train = f['x_train'], f['y_train']
     x_test, y_test = f['x_test'], f['y_test']
